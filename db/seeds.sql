bb4b7e5063c48d6e7ce786c9287544e81318af6e
Joe Wills <JGW3Code@gmail.com>
2020-06-30 00:32:56 -0500

    late night confab

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("King Me");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Me");
+INSERT INTO burgers (burger_name) VALUES ("Cheese Me");
