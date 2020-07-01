5b884ecf1721383832d6798f88f08bddbfe20027
Joe Wills <JGW3Code@gmail.com>
2020-07-01 00:07:37 -0500

    a new burger

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up My SQL connection
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "",
     database: "burgers_db"
 });
+}
 
 // Establish connection
 connection.connect(function(err){
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name) VALUES ("King Me");
-INSERT INTO burgers (burger_name) VALUES ("Bacon Me");
-INSERT INTO burgers (burger_name) VALUES ("Cheese Me");
+INSERT INTO burgers (burger_name) VALUES ("Animal Style");
+INSERT INTO burgers (burger_name) VALUES ("Cobra Kai");
+INSERT INTO burgers (burger_name) VALUES ("Cowboy");
