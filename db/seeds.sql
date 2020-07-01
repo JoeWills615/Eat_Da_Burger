58e5e5445d82b109a461bec4c3b2a594a19a6201
Joe Wills <JGW3Code@gmail.com>
2020-07-01 01:16:03 -0500

    feed me Seymour

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name) VALUES ("Animal Style");
-INSERT INTO burgers (burger_name) VALUES ("Cobra Kai");
-INSERT INTO burgers (burger_name) VALUES ("Cowboy");
+INSERT INTO burgers (burger_name) VALUES ("Kevin Bacon Burger");
+INSERT INTO burgers (burger_name) VALUES ("Say Extra Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Mission Impossible Burger");
